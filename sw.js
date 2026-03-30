182b7d3d9401ac2dc4405513315c0b60047077be
nationsskyreports-rgb <nations.sky.reports@gmail.com>
2026-03-30 17:06:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'align-v1';
-const FILES = ['/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const FILES = ['/Enjy-Flow/index.html', '/Enjy-Flow/manifest.json', '/Enjy-Flow/icon-192.png', '/Enjy-Flow/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
